1c5d52dadf9fdae9c34f2a436267f027d9018057
Madjar Evan <evanmadjar@gmail.com>
2024-01-30 13:58:03 +0200

    chore: Update main.py file with DB connection

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from cassandra.cqlengine.management import sync_table
+
+from . import db
+from .users.models import User
 
 
-main_app = FastAPI()
+app = FastAPI()
 
 
-@main_app.get('/')
-def homepage():
-    return {'hello': ''}
+@app.on_event('startup')
+def on_startup():
+    global DB_SESSION
+    DB_SESSION = db.get_session()
+    sync_table(User)
+
+
+@app.get('/')
+def homepage() -> dict:
+    return {'hello': '',}
