2b9459dc13697cf2c526e393df859d8860fbe6b5
Madjar Evan <evanmadjar@gmail.com>
2024-02-03 17:31:54 +0200

    chore: Update settings with JWT and dirs

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from functools import lru_cache
 from pydantic import Field
 from pydantic_settings import (
@@ -11,10 +12,13 @@ os.environ['CQLENG_ALLOW_SCHEMA_MANAGEMENT'] = '1'
 
 
 class Settings(BaseSettings):
+    base_dir: Path = Path(__file__).resolve().parent
+    templates_dir: Path = Path(__file__).resolve().parent / 'templates'
     keyspace: str = Field(..., alias='ASTRADB_KEYSPACE')
     db_client_id: str = Field(..., alias='ASTRADB_CLIENT_ID')
     db_client_secret: str = Field(..., alias='ASTRADB_CLIENT_SECRET')
-
+    secret_key: str = Field(..., alias='SECRET_KEY')
+    jwt_algorithm: str = Field(default='HS256')
     model_config = SettingsConfigDict(env_file='.env')
 
 
