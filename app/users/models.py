0277bf9e89fd68da94b63c153c17f75f8ceaf4d7
Madjar Evan <evanmadjar@gmail.com>
2024-02-05 15:17:48 +0200

    feat: Add exceptions and check methods to User

--- app/users/models.py
+++ app/users/models.py
@@ -1,9 +1,15 @@
 import uuid
+from uuid import UUID
+from typing import Optional
 from cassandra.cqlengine import columns
 from cassandra.cqlengine.models import Model
 from app.config import get_settings
 
 from . import validators, security
+from .exceptions import (
+    UserHasAccountException,
+    InvalidEmailException,
+)
 
 settings = get_settings()
 
@@ -18,10 +24,10 @@ class User(Model):
     password = columns.Text()
 
     def __str__(self) -> str:
-        return f'User(emial={self.email}, user_id={self.user_id})'
+        return f'User(email={self.email}, user_id={self.user_id})'
 
     def __repr__(self) -> str:
-        return f'User(emial="{self.email}", user_id="{self.user_id}")'
+        return f'User(email="{self.email}", user_id="{self.user_id}")'
 
     def set_password(self, pw, commit=False):
         pw_hash = security.generate_hash(pw)
@@ -39,13 +45,27 @@ class User(Model):
     def create_user(email, password=None):
         q = User.objects.filter(email=email)
         if q.count() != 0:
-            raise Exception('User already has an account.')
+            raise UserHasAccountException('User already has an account.')
 
         valid, msg, email = validators._validate_email(email)
         if not valid:
-            raise Exception(f'Invalid email: {msg}')
+            raise InvalidEmailException(f'Invalid email: {msg}')
 
         obj = User(email=email)
         obj.set_password(password)
         obj.save()
         return obj
+
+    @staticmethod
+    def check_exists(user_id: UUID) -> bool:
+        q = User.objects.filter(user_id=user_id).allow_filtering()
+        return q.count() != 0
+
+    @staticmethod
+    def by_user_id(user_id: Optional[UUID]) -> 'User':
+        if user_id is None:
+            return None
+        q = User.objects.filter(user_id=user_id).allow_filtering()
+        if q.count() != 1:
+            return None
+        return q.first()
