21ff412cac9a2b778b67c238d4a9391818bf807a
Madjar Evan <evanmadjar@gmail.com>
2024-02-05 15:17:28 +0200

    chore: Create custom exceptions classes

--- app/users/exceptions.py
+++ app/users/exceptions.py
@@ -3,3 +3,15 @@ from fastapi import HTTPException
 
 class LoginRequiredException(HTTPException):
     pass
+
+
+class InvalidUserIDException(HTTPException):
+    pass
+
+
+class UserHasAccountException(Exception):
+    pass
+
+
+class InvalidEmailException(Exception):
+    pass
