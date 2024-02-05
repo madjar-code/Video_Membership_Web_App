84302a3902d404f416a133c4f4f9d055c8e07c99
Madjar Evan <evanmadjar@gmail.com>
2024-02-05 15:19:08 +0200

    chore: Create custom exceptions in videos module

---
+++ app/videos/exceptions.py
@@ -0,0 +1,10 @@
+class InvalidYoutubeVideoURLException(Exception):
+    """
+    Invalid Youtube Video URL
+    """
+
+
+class VideoAlreadyAddedException(Exception):
+    """
+    Video already added
+    """
